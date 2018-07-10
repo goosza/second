1905dd7c447a2c66689830bcfabee8efa185ef18
natashochek <neobutovann@gmail.com>
2018-07-10 17:31:12 +0900

    text

--- resources/views/pictures.blade.php
+++ resources/views/pictures.blade.php
@@ -1,4 +1,12 @@
 @extends("layout")
 @section("body")
-    
+    <div class="container">
+        <div class="row">
+            <div class="col" style="overflow:hidden; height:200px;"><img src="/img/1.jpg" alt=""></div>
+            <div class="col" style="overflow:hidden; height:200px;"><img src="/img/2.jpg" alt=""></div>
+            <div class="col" style="overflow:hidden; height:200px;"><img src="/img/3.jpg" alt=""></div>
+            <div class="col" style="overflow:hidden; height:200px;"><img src="/img/4.jpg" alt=""></div>
+            <div class="col" style="overflow:hidden; height:200px;"><img src="/img/5.jpg" alt=""></div>
+        </div>
+    </div>
 @endsection
