a809ef819240d5ffeefd813f79cb168b21f6a765
goosza <vellenore@gmail.com>
2018-07-10 17:20:35 +0900

    text

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <!DOCTYPE>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <style>
+            .footer{
+                height:15%;
+                position:absolute;
+                bottom:0;
+                width:100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          
+          
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                <a class="nav-link" href="#">Лол<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Аян<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Мелкий</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Лох</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Совет лицея
+                </a>
+
+              </li>
+            </ul>
+          </div>
+        </nav>
+        
+        </div>
+       <div class="content">
+            @yield("body")
+       </div>
+        
+        
+        <nav class="navbar navbar-light bg-light footer">
+          <a class="navbar-brand" href="#">
+            <img src="/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Bootstrap
+          </a>
+        </nav>
+
+
+ 
+    </body>
+</html>
+    </body>
+</html>
